1d00b1972fc9d3b6a37b283c2fd2e0e4c6ab4570
AleVargas01 <116041681+AleVargas01@users.noreply.github.com>
2022-10-25 22:02:33 -0300

    Add files via upload

--- ejercicios_practica/ejercicio_1.py
+++ ejercicios_practica/ejercicio_1.py
@@ -18,12 +18,16 @@ numero_2 = 7
 # ej:
 # suma = .....
 
+suma = numero_1 + numero_2
+
 # Imprimir en pantalla el resultado de la suma
 # print(....)
-
+print ('El resultado de la suma entre',numero_1, 'y',numero_2, 'es:',suma)
 # Repita el procedimiento para realizar la resta
 # Crear una variable llamada resta donde se 
 # almacene el valor de la resta de numero_1 menos numero_2
 
+resta= numero_1 - numero_2
 
 # Imprimir en pantalla el resultado de la resta
+print('El resultado de la resta entre',numero_1,'y',numero_2,'es:',resta)
--- ejercicios_practica/ejercicio_3.py
+++ ejercicios_practica/ejercicio_3.py
@@ -19,14 +19,15 @@ apellido = str(input('Ingrese por consola su primer apellido:'))
 
 # Crear una variable llamada nombre_apellido donde se 
 # almacene el contenido de las variables nombre y apellido
-# separando con un nespacio su nombre de su apellido
+# separando con un espacio su nombre de su apellido
 
 # nombre_apellido = .....
-
+nombre_apellido= nombre +' '+ apellido
 # Crear una variable llamada cantidad donde se
 # almacene la cantidad de caracteres que posee la variable
 # nombre_apellido utilizando la función len
 
 # cantidad = len(....)
-
+cantidad =len(nombre_apellido)
 # Imprimir en pantalla la variable cantidad
+print('La cantidad de caracteres es:',cantidad)
